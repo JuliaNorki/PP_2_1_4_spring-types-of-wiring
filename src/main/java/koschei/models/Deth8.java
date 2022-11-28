6d651a5a36c584caaaad99198260241dbfc4ddc7
JuliaNorki <109559162+JuliaNorki@users.noreply.github.com>
2022-11-27 21:48:37 -0800

    1

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,8 @@
 package koschei.models;
 
 public class Deth8 {
+    public String toString() {
+        return "";
+    }
+
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,10 @@
 package koschei.models;
 
 public class Needle7 {
+    private Deth8 deth8;
 
     @Override
-    public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+    public  String toString() {
+        return ", смерть Кощея на игле :( " + deth8.toString() +"";
     }
 }
